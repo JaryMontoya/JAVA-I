179e112873c22673957e18b2e0135615d3c5587c
[JaryMontoya] <[jarycarolinam125@gmail.com]>
2024-01-18 21:17:00 -0500

    JaryMontoya

--- src/main/java/B4/Ejercicio2.java
+++ src/main/java/B4/Ejercicio2.java
@@ -4,6 +4,8 @@ import java.util.Scanner;
 
 public class Ejercicio2 {
     public static void main(String[] args) {
+        double []= ordename=new double [5];
+
         Scanner scanner = new Scanner(System.in);
         System.out.println("Ingrese una nota de 1-5");
 
