acb389cd38b339d9c86a38d8dc2638c78898661e
[JaryMontoya] <[jarycarolinam125@gmail.com]>
2024-01-18 21:12:56 -0500

    JaryMontoya

--- src/main/java/B4/Ejercicio2.java
+++ src/main/java/B4/Ejercicio2.java
@@ -1,4 +1,18 @@
 package B4;
 
+import java.util.Scanner;
+
 public class Ejercicio2 {
-}
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ingrese una nota de 1-5");
+
+        // Leer la entrada del usuario como un entero (int)
+        int nota = scanner.nextInt();
+
+        scanner.close();
+
+
+    }
+    }
+
