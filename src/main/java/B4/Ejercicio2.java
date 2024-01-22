0e2f5a2266c5a9a56e76c0304c6639afed69ef6f
[JaryMontoya] <[jarycarolinam125@gmail.com]>
2024-01-22 18:29:59 -0500

    JaryMontoya

--- src/main/java/B4/Ejercicio2.java
+++ src/main/java/B4/Ejercicio2.java
@@ -1,20 +1,26 @@
 package B4;
 
+
 import java.util.Scanner;
 
+//Calcular el promedio de 5 datos agragados, notas de 1 a 5
 public class Ejercicio2 {
+
     public static void main(String[] args) {
-        double[] ordename=new double [5];
+        double[] ordename=new double[5];
 
+        double contador=0;
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Ingrese una nota de 1-5");
 
-        // Leer la entrada del usuario como un entero (int)
-        int nota = scanner.nextInt();
+        for (int i =0;i<ordename.length;i++){
+            System.out.print("Ingrese su Nota");
+            ordename[i]=scanner.nextDouble();
+        }
 
-        scanner.close();
+        for (int i =0;i<ordename.length-1;i++){
+            contador= contador+ordename[i];
+        }
 
-
+        System.out.println(contador/ordename.length);
     }
-    }
-
+}
